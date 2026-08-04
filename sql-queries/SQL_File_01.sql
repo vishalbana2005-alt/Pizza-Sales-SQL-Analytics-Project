57d6705b74de65acf28936d7858319c0604a4f4a
vishalbana2005-alt <vishalbana2005@gmail.com>
2026-08-04 13:03:05 +0530

    Add files via upload

---
+++ sql-queries/SQL_File_01.sql
@@ -0,0 +1,3 @@
+-- Retrieve the total number of orders placed.
+
+SELECT COUNT(order_id) AS total_orders FROM orders;
---
+++ sql-queries/SQL_File_08.sql
@@ -0,0 +1,6 @@
+-- Join relevant tables to find the 
+-- category-wise distribution of pizzas.
+
+
+SELECT category, COUNT(name) FROM pizza_types
+GROUP BY category;
